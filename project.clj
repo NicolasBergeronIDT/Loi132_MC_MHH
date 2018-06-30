54e2152c534581f2c7359d907c499265848d7e87
nicber <nbergeron@idt.quebec>
2018-06-30 15:57:10 -0400

    add txt

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject loi132 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Compensation pour pour l'atteinte aux milieux humides
+  et hydriques. Projet de règlement. Loi sur la qualité de
+  l'environnement (chapitre Q-2). Gazette officelle du Québec (23 mai
+  2018) "
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
